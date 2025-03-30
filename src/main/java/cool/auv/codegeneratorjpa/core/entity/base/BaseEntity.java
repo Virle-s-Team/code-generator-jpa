4693d754c034d724733962ebb273920a14c9d4b9
virle <geyingauv@gmail.com>
2025-03-30 09:14:25 +0800

    feat 审计字段

--- src/main/java/cool/auv/codegeneratorjpa/core/entity/base/BaseEntity.java
+++ src/main/java/cool/auv/codegeneratorjpa/core/entity/base/BaseEntity.java
@@ -1,9 +1,15 @@
 package cool.auv.codegeneratorjpa.core.entity.base;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Data;
 import lombok.experimental.Accessors;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.io.Serializable;
@@ -12,16 +18,19 @@ import java.time.LocalDateTime;
 @Data
 @Accessors(chain = true)
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity implements Serializable {
 
     /**
      * 创建人
      */
+    @CreatedBy
     private String createBy;
 
     /**
      * 创建时间
      */
+    @CreatedDate
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createTime;
@@ -29,11 +38,13 @@ public class BaseEntity implements Serializable {
     /**
      * 更新人
      */
+    @LastModifiedBy
     private String updateBy;
 
     /**
      * 更新时间
      */
+    @LastModifiedDate
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime updateTime;
